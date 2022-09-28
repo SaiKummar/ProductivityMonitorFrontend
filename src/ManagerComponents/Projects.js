af4e8197c45f3f8f5d8dfdd839ab690e4737c056
Sai Kumar <saikummar94@gmail.com>
2022-09-28 16:39:23 +0530

    display projects

--- src/ManagerComponents/Projects.js
+++ src/ManagerComponents/Projects.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import './Projects.css';
 
 export default function Projects() {
+    const [pdata, setPData] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:5179/api/Manager/v1/Projects")
+            .then(response => response.json())
+            .then(data => { console.log(data); setPData(data); })
+    }, []);
+
     return (
         <div>
-            <h3>Projects</h3>
+            <h2>My Projects</h2>
+            <div className='projectsflexbox'>
+                {
+                    pdata.map(project =>
+                        <div className="projectcontainer">
+                            <h2>{project.projectName}</h2>
+                            <p>{project.projectDescription}</p>
+                        </div>
+                    )
+                }
+            </div>
         </div>
     );
 }
